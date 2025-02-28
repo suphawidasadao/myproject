85816e24ae69f50f7231c4e37ed91cce0ea16714
Suphawida <66022983@up.ac.th>
2025-02-28 13:27:31 +0700

    authProvider

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import { AuthProvider } from "./Providers";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -20,9 +21,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <AuthProvider>
+            {children}
+          </AuthProvider>
+        
       </body>
     </html>
   );
