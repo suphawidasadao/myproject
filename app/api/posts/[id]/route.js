4c3c426e350860fb1fb718b401718a993d132a9f
Suphawida <66022983@up.ac.th>
2025-02-28 15:39:20 +0700

    post updated

--- app/api/posts/[id]/route.js
+++ app/api/posts/[id]/route.js
@@ -8,3 +8,11 @@ export async function GET(req, { params }) {
     const post = await Post.findOne({ _id: id })
     return NextResponse.json({ post }, { status: 200})
 }
+
+export async function PUT(req, { params }) {
+    const { id } = params;
+    const { newname: name, newtype: type, newtransmission: transmission, newseats: seats, newfuel: fuel, newengine: engine, newdoors: doors, newluggage: luggage, newprice: price, newprovince: providers, newimages: images, newlocations: locations, newfeatures: features, newsafety: safety} = await req.json();
+    await connectMongoDB();
+    await Post.findByIdAndUpdate(id, { name, type, transmission, seats, fuel, engine, doors, luggage, price, providers, images, locations, features, safety });
+    return NextResponse.json({ message: "Post updated"}, { status: 200})
+}
