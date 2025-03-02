e9dd23464c28b83b9c999e802ad02a0db38699a2
Suphawida <66022983@up.ac.th>
2025-03-03 03:06:50 +0700

    logo footer

--- app/components/footer.js
+++ app/components/footer.js
@@ -1,8 +1,8 @@
 "use client";
- 
+
 import Image from 'next/image';
 import React from "react";
- 
+
 export default function Footer() {
     return (
         <div className="flex flex-col ">
@@ -10,7 +10,7 @@ export default function Footer() {
             <div className="flex-grow">
                 {/* Your main content goes here */}
             </div>
- 
+
             {/* Footer */}
             <footer className="bg-gray-800 text-white flex justify-center">
                 <div className="container mx-auto px-24 py-6">
@@ -19,7 +19,7 @@ export default function Footer() {
                         <div className="-translate-y-8">
                             <h2 className="text-xl font-semibold mb-4 flex items-center space-x-2">
                                 <Image
-                                    src="logocar.svg"
+                                    src="/logocar.svg"
                                     alt="Logo"
                                     width={100}
                                     height={100}
@@ -27,9 +27,9 @@ export default function Footer() {
                                 />
                                 <span className="text-sm tracking-wider">DRIVEFLEX</span>
                             </h2>
- 
+
                             <p className="text-sm -translate-y-8 ml-5">University of Phayao</p>
- 
+
                             <div className="text-sm mt-4 flex items-center space-x-4 -translate-y-5 ml-5">
                                 <span className="flex items-center space-x-1">
                                     <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -37,7 +37,7 @@ export default function Footer() {
                                     </svg>
                                     <span>01-234-5678</span>
                                 </span>
- 
+
                                 <span className="flex items-center space-x-1">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="20"
                                         height="20" viewBox="0 0 512 512"><path fill="#ffffff" d="M311 196.8v81.3c0 2.1-1.6 3.7-3.7 3.7h-13c-1.3 0-2.4-.7-3-1.5l-37.3-50.3v48.2c0 2.1-1.6 3.7-3.7 3.7h-13c-2.1 0-3.7-1.6-3.7-3.7V196.9c0-2.1 1.6-3.7 3.7-3.7h12.9c1.1 0 2.4 .6 3 1.6l37.3 50.3V196.9c0-2.1 1.6-3.7 3.7-3.7h13c2.1-.1 3.8 1.6 3.8 3.5zm-93.7-3.7h-13c-2.1 0-3.7 1.6-3.7 3.7v81.3c0 2.1 1.6 3.7 3.7 3.7h13c2.1 0 3.7-1.6 3.7-3.7V196.8c0-1.9-1.6-3.7-3.7-3.7zm-31.4 68.1H150.3V196.8c0-2.1-1.6-3.7-3.7-3.7h-13c-2.1 0-3.7 1.6-3.7 3.7v81.3c0 1 .3 1.8 1 2.5c.7 .6 1.5 1 2.5 1h52.2c2.1 0 3.7-1.6 3.7-3.7v-13c0-1.9-1.6-3.7-3.5-3.7zm193.7-68.1H327.3c-1.9 0-3.7 1.6-3.7 3.7v81.3c0 1.9 1.6 3.7 3.7 3.7h52.2c2.1 0 3.7-1.6 3.7-3.7V265c0-2.1-1.6-3.7-3.7-3.7H344V247.7h35.5c2.1 0 3.7-1.6 3.7-3.7V230.9c0-2.1-1.6-3.7-3.7-3.7H344V213.5h35.5c2.1 0 3.7-1.6 3.7-3.7v-13c-.1-1.9-1.7-3.7-3.7-3.7zM512 93.4V419.4c-.1 51.2-42.1 92.7-93.4 92.6H92.6C41.4 511.9-.1 469.8 0 418.6V92.6C.1 41.4 42.2-.1 93.4 0H419.4c51.2 .1 92.7 42.1 92.6 93.4zM441.6 233.5c0-83.4-83.7-151.3-186.4-151.3s-186.4 67.9-186.4 151.3c0 74.7 66.3 137.4 155.9 149.3c21.8 4.7 19.3 12.7 14.4 42.1c-.8 4.7-3.8 18.4 16.1 10.1s107.3-63.2 146.5-108.2c27-29.7 39.9-59.8 39.9-93.1z" /></svg>
@@ -45,8 +45,8 @@ export default function Footer() {
                                 </span>
                             </div>
                         </div>
- 
- 
+
+
                         <div className="pl-14">
                             <h3 className="text-lg font-semibold mb-4">ติดตามเรา</h3>
                             <div className="flex space-x-4">
@@ -61,7 +61,7 @@ export default function Footer() {
                                 </svg>
                             </div>
                         </div>
- 
+
                         {/* Help Section */}
                         <div>
                             <h3 className="text-lg font-semibold mb-4 items-center">ความช่วยเหลือ</h3>
@@ -75,4 +75,4 @@ export default function Footer() {
         </div>
     );
 }
- 
+
